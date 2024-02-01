cb4b72bf4e267d99bce3121a14ee88cbdd1da7c4
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-02-01 10:07:33 +0100

    Add files via upload

---
+++ hw_fe_java_31012024/script.js
@@ -0,0 +1,26 @@
+// Написать программу, которая через prompt считывает число и выводит в консоль число равное 10% от введенного числа
+
+// const num = +prompt ("gib eine Zahl ein")
+// console.log(num / 100*10)
+
+// Написать программу, которая получает два числа и выводит наименьшее
+
+// const num1 = 4
+// const num2 = 25
+// if(num1 > num2){console.log(num2)
+// }else {console.log(num1)}
+
+// Написать программу, которая считывает через prompt число и выводит одно из следующих сообщений: ‘Число меньше 100’, ‘Число больше 100’ или ‘Число равно 100’
+
+// const num = +prompt ("gib eine Zahl ein")
+// if (num > 100){console.log("Zahl ist größer als 100")
+// } else if (num < 100){console.log("Zahl ist kleiner als 100")}
+// else{console.log("Zahl ist gleich 100")}
+
+// Написать программу, которая запрашивает у пользователя его имя и возраст (в годах) и выводит в консоль сообщение ‘Hello, <name>’, если пользователь совершеннолетний, или ‘Hi, <name>’, если пользователь несовершеннолетний.
+// const username = prompt ("Geben Sie Ihr Name ein")
+// const userbirthyear = +prompt ("Geben Sie Ihr Geburtsjahr ein")
+// if (2024 - userbirthyear > 18){console.log(`Hallo ${username}`)
+// }else {console.log(`Hi ${username}`)
+// } 
+
